982411c5f07751164462ca81b422411781b6351c
Tino-tee <chitimatinotenda55@gmail.com>
2026-06-11 16:06:20 +0200

    Leave Management CRUD completion

--- Leave _Management_System/Data/ApplicationDbContext.cs
+++ Leave _Management_System/Data/ApplicationDbContext.cs
@@ -3,7 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Leave__Management_System.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    public class ApplicationDbContext : IdentityDbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LeaveType> LeaveTypes { get; set; }
     }
 }
